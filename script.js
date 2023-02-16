0709113d5281ec2ebc87cd0ea107e36bf2656402
Hunter Zepp <122405685+HunterZepp24@users.noreply.github.com>
2023-02-16 05:53:04 +0000

    Updated GSAP

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 // Check JS is Connected
 console.log('JS Connected');
 
-gsap.fromTo(
+let tl1 = gsap.timeline({ repeat: -1, repeatDelay: 5.5 });
+let tl2 = gsap.timeline({ repeat: -1, repeatDelay: 2.5 });
+let tl3 = gsap.timeline({ repeat: -1, repeatDelay: 1.5 });
+let tl4 = gsap.timeline({ repeat: -1, repeatDelay: 2.5 });
+
+tl1
+  .fromTo(
     ".jaws",
     {
       x: 600
@@ -9,10 +15,23 @@ gsap.fromTo(
     {
       x: -350,
       duration: 4.5,
-      ease: "none"
+      ease: "none",
+      delay: 1
     }
-);
-gsap.fromTo(
+  )
+  .fromTo(
+    ".harry-potter",
+    {
+      x: 225
+    },
+    {
+      x: -400,
+      duration: 1,
+      ease: "none"
+    },
+    "-=3.5"
+  )
+  .fromTo(
     ".et",
     {
       x: -150,
@@ -22,39 +41,48 @@ gsap.fromTo(
       x: 250,
       y: -55,
       duration: 2,
-      ease: "none",
-      delay: 1.5
-    }
-);
-gsap.fromTo(
-    ".harry-potter",
+      ease: "none"
+    },
+    "-=3"
+  )
+  .fromTo(
+    ".luke-saber",
     {
-      x: 225
+      opacity: 0
     },
     {
-      x: -400,
-      duration: 1,
-      ease: "none",
-      delay: 1
-    }
-);
-gsap.to(".superman-standing-1", {
+      opacity: 1,
+      duration: 1.5
+    },
+    "-=2.5"
+  )
+  .fromTo(
+    ".vader-saber",
+    {
+      opacity: 0
+    },
+    {
+      opacity: 1,
+      duration: 1.5
+    },
+    "-=1.5"
+  );
+tl2
+  .to(".superman-standing-1", {
     opacity: 0,
     duration: 0,
-    delay: 3
-});
-gsap.to(".superman-flight", {
+    delay: 3.5
+  })
+  .to(".superman-flight", {
     opacity: 1,
-    duration: 0,
-    delay: 3
-});
-gsap.to(".superman-flight", {
+    duration: 0
+  })
+  .to(".superman-flight", {
     y: -500,
     duration: 0.5,
-    delay: 3,
     ease: "power1.in"
-});
-gsap.fromTo(
+  })
+  .fromTo(
     ".superman-standing-2",
     {
       x: -550
@@ -62,11 +90,12 @@ gsap.fromTo(
     {
       x: 0,
       duration: 0.5,
-      delay: 6,
+      delay: 4,
       ease: "power2.out"
     }
-);
-gsap.fromTo(
+  );
+tl3
+  .fromTo(
     ".close-encounters",
     {
       y: -200
@@ -74,10 +103,11 @@ gsap.fromTo(
     {
       y: 0,
       duration: 1,
-      delay: 3
+      delay: 4.5,
+      ease: "power1.inOut"
     }
-);
-gsap.fromTo(
+  )
+  .fromTo(
     "#beam",
     {
       opacity: 0
@@ -85,69 +115,51 @@ gsap.fromTo(
     {
       opacity: 1,
       duration: 1,
-      delay: 4
+      ease: "power1.inOut"
     }
-);
-gsap.to("#beam", {
+  )
+  .to(".jurassic-park", {
     opacity: 0,
     duration: 1,
-    delay: 6
-});
-gsap.to(".close-encounters", {
+    ease: "power1.inOut"
+  })
+  .to("#beam", {
+    opacity: 0,
+    duration: 1,
+    ease: "power1.inOut"
+  })
+  .to(".close-encounters", {
     y: -200,
     duration: 1,
-    delay: 7
-});
-gsap.to(".jurassic-park", {
-    opacity: 0,
-    duration: 1,
-    delay: 5
-});
-gsap.fromTo(
-    ".luke-saber",
-    {
-      opacity: 0
-    },
-    {
-      opacity: 1,
-      duration: 1.5,
-      delay: 1.5
-    }
-);
-gsap.fromTo(
-    ".vader-saber",
-    {
-      opacity: 0
-    },
-    {
-      opacity: 1,
-      duration: 1.5,
-      delay: 3
-    }
-);
-gsap.to("#hand-ij", {
-    rotate: "20deg",
-    y: 5,
-    x: 5,
-    duration: 1.5,
-    repeat: -1,
-    yoyo: true,
-    repeatDelay: 0.25,
     ease: "power1.inOut"
-});
-gsap.from(".left-arm-ha", {
+  });
+tl4
+  .from(".left-arm-ha", {
     rotate: "20deg",
     duration: 0.1,
     x: 40,
     y: 75,
-    delay: 7.5,
+    delay: 8.5,
     ease: "none"
-});
-gsap.from(".right-arm-ha", {
-    rotate: "-20deg",
-    duration: 0.1,
-    x: -35,
-    y: 100,
-    delay: 7.5,
-    ease: "none"
+  })
+  .from(
+    ".right-arm-ha",
+    {
+      rotate: "-20deg",
+      duration: 0.1,
+      x: -35,
+      y: 100,
+      ease: "none"
+    },
+    "-=.1"
+  );
+gsap.to("#hand-ij", {
+  rotate: "20deg",
+  y: 5,
+  x: 5,
+  duration: 1.5,
+  repeat: -1,
+  yoyo: true,
+  repeatDelay: 0.25,
+  ease: "power1.inOut"
 });
